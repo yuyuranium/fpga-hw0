85da70b79f9a48a4815c7a35573ec29b932e0821
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-02-28 16:18:12 +0800

    Update SET.v

--- SET.v
+++ SET.v
@@ -1,3 +1,7 @@
+`include "PE.v"
+`include "Analyzer.v"
+`include "Controller.v"
+
 module SET (
     input        clk,
     input        rst,
@@ -10,14 +14,39 @@ module SET (
     output [7:0] candidate
 );
 
+    reg  [3:0] x_a, y_a, x_b, y_b, x_c, y_c;
+    reg  [3:0] r_a, r_b, r_c;
     wire [1:0] stage;
-    wire [3:0] x_a = central[23:20], y_a = central[19:16],
-               x_b = central[15:12], y_b = central[11:8],
-               x_c = central[7:4], y_c = central[3:0];
-    wire [3:0] r_a = radius[11:8], r_b = radius[7:4], r_c = radius[3:0];
     wire [3:0] x, y;
     wire a, b, c;
     wire analyze_en;
+
+    /* Registers for central and radius of A, B and C */
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            x_a <= 3'd0;
+            y_a <= 3'd0;
+            x_b <= 3'd0;
+            y_b <= 3'd0;
+            x_c <= 3'd0;
+            y_c <= 3'd0;
+            r_a <= 3'd0;
+            r_b <= 3'd0;
+            r_c <= 3'd0;
+        end else begin
+            if (en) begin
+                x_a <= central[23:20];
+                y_a <= central[19:16];
+                x_b <= central[15:12];
+                y_b <= central[11:8];
+                x_c <= central[7:4]; 
+                y_c <= central[3:0];
+                r_a <= radius[11:8];
+                r_b <= radius[7:4];
+                r_c <= radius[3:0];
+            end
+        end
+    end
 
     Controller ctrl(
         .clk(clk),
@@ -69,11 +98,13 @@ module SET (
     Analyzer analyzer(
         .clk(clk),
         .rst(rst),
+        .en(en),
         .analyze_en(analyze_en),
         .mode(mode),
         .A(a),
         .B(b),
         .C(c),
         .res(candidate)
-    )
+    );
+
 endmodule
