f6cdc38d494bcef970aa46ca5b8da3b61636c51b
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-03-08 11:11:07 +0800

    Add test case for SETtb.v

--- SETtb.v
+++ SETtb.v
@@ -83,6 +83,21 @@ module testbench;
     radius = 12'd0;
     mode = 2'd0;
 
+    //transmit the third signal about central, radius, and mode
+    wait (busy == 1'b0) begin
+      @(negedge clk) begin
+        en = 1'b1;
+        central = 24'b0101_0101_0011_0011_0110_0010;
+        radius = 12'b0011_0011_0010;
+        mode = 2'b11;
+      end
+    end
+
+    #10 en = 1'b0;
+    central = 24'd0;
+    radius = 12'd0;
+    mode = 2'd0;
+
     @(negedge valid) begin
       $display("The simulation is done!");
       $finish;
